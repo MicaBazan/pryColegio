ef33122e2931761a9ed4c984cfc0f582c43c3e6c
Micaela Bazan <micabazan5@gmail.com>
2023-05-03 17:59:08 -0300

    clsConexion

--- clsAlumno.cs
+++ clsAlumno.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Data.OleDb;
 
 namespace pryColegio
 {
     class clsAlumno
     {
+        OleDbConnection conector;
+        OleDbCommand comando;
+        OleDbDataAdapter adaptador;
+        DataTable tabla;
+
+        
     }
 }
