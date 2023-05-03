7429506a01cbee8275a243c3a9d2357e5566f3f1
Micaela Bazán <83982664+MicaBazan@users.noreply.github.com>
2023-05-03 17:35:34 -0300

    Add files via upload

---
+++ frmPrincipal.Designer.cs
@@ -0,0 +1,48 @@
+﻿
+namespace pryColegio
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(724, 417);
+            this.Name = "frmPrincipal";
+            this.Text = "Principal";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
